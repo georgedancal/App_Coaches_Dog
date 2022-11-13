f2a9d0f7f0f8a523f3a7088a392117a0c8d554a4
georgedancal <gedanc@gmail.com>
2022-11-13 16:44:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appcoachesdog/view/ui/activities/Loguin.kt
@@ -0,0 +1,12 @@
+package com.example.appcoachesdog.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appcoachesdog.R
+
+class Loguin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loguin)
+    }
+}
