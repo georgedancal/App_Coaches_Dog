762b2282434b687c7b0952ac57124febeeae7a7e
georgedancal <gedanc@gmail.com>
2022-11-18 08:43:52 -0500

    splash

--- app/src/main/java/com/example/appcoachesdog/view/ui/activities/Splash.kt
+++ app/src/main/java/com/example/appcoachesdog/view/ui/activities/Splash.kt
@@ -1,12 +1,30 @@
 package com.example.appcoachesdog.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.appcoachesdog.R
+import com.example.appcoachesdog.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
+    lateinit var binding : ActivitySplashBinding
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animationView.setAnimation(R.raw.dog)
+        binding.animationView.playAnimation()
+
+        handler= Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
     }
 }
